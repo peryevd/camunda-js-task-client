6a4cfd077f969b327f9cae31d15143c9b8494052
peryevd <peryevd@rambler.ru>
2022-06-28 15:09:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,25 @@ const { Variables } = require('camunda-external-task-client-js')
 const client = new Client(config)
 client.subscribe('GetCatFact', async function ({ task, taskService }) {
   const processVariables = new Variables()
-  console.log('Message from client 1')
-//   axios.get('https://catfact.ninja/fact').then(response => {
-//     processVariables.set('catFact', response.data.fact)
-//     console.log(response.data.fact)
-//   })
-  taskService.complete(task, processVariables)
+
+  axios.get('https://catfact.ninja/fact').then(response => {
+    processVariables.set('catFact', response.data.fact)
+    console.log(response.data.fact)
+    taskService.complete(task, processVariables)
+  })
 })
 
 const client2 = new Client(config)
 client2.subscribe('SendTG', async function ({ task, taskService }) {
+  console.log('start sent')
 
-//   const catFact = task.variables.get('catFact')
-  console.log('Message from client 2')
-
-//   axios.get(`https://api.telegram.org/yourbot=${catFact}`).then(response => {
-//     //   processVariables.set('docFact', response.data.fact)
-//     console.log('sent')
-//   })
+  const catFact = task.variables.get('catFact')
+  console.log('==============', catFact)
   taskService.complete(task)
-  
+
+  // axios.get(`https://api.telegram.org/yourbot=${catFact}`).then(response => {
+  //   //   processVariables.set('docFact', response.data.fact)
+  //   console.log('sent')
+  //   taskService.complete(task)
+  // })
 })
